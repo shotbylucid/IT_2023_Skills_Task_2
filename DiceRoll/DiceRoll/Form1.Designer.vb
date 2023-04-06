494d03b51feea0141827e2249f5944a172fb1cf4
Lucid <lucidshq@gmail.com>
2023-04-06 23:53:48 +0930

    Update - New Features (Ability to choose Bet Amount, Number of Rolls & More)
    Added:
    - Ability to choose how many roles you want from a pop down menu
    - Ability to choose what number your rolling for
    - Added the ability for you to choose how much you bet
    
    Fixes/ Changes:
    - Changed tbx_Count to tbx_Money and made it show how much money you win each round
    - Changed tbx_Cash to tbx_Bank
    - Removed Dim JackPot and * of the count
    
    - Difficulty system so if you add more risk for yourself you get a bigger reward

--- DiceRoll/DiceRoll/Form1.Designer.vb
+++ DiceRoll/DiceRoll/Form1.Designer.vb
@@ -24,10 +24,14 @@ Partial Class Form1
     Private Sub InitializeComponent()
         btn_Roll = New Button()
         lsb_Dice = New ListBox()
-        tbx_Count = New TextBox()
-        tbx_Money = New TextBox()
+        tbx_Cash = New TextBox()
+        tbx_Bank = New TextBox()
         btn_Reset = New Button()
         btn_Close = New Button()
+        tbx_BetAmount = New TextBox()
+        cmb_RollValue = New ComboBox()
+        cmb_RollAmount = New ComboBox()
+        cmb_DifficultyLevel = New ComboBox()
         SuspendLayout()
         ' 
         ' btn_Roll
@@ -48,19 +52,19 @@ Partial Class Form1
         lsb_Dice.Size = New Size(443, 740)
         lsb_Dice.TabIndex = 1
         ' 
-        ' tbx_Count
+        ' tbx_Cash
         ' 
-        tbx_Count.Location = New Point(370, 948)
-        tbx_Count.Name = "tbx_Count"
-        tbx_Count.Size = New Size(200, 39)
-        tbx_Count.TabIndex = 2
+        tbx_Cash.Location = New Point(370, 948)
+        tbx_Cash.Name = "tbx_Cash"
+        tbx_Cash.Size = New Size(200, 39)
+        tbx_Cash.TabIndex = 2
         ' 
-        ' tbx_Money
+        ' tbx_Bank
         ' 
-        tbx_Money.Location = New Point(370, 1033)
-        tbx_Money.Name = "tbx_Money"
-        tbx_Money.Size = New Size(200, 39)
-        tbx_Money.TabIndex = 3
+        tbx_Bank.Location = New Point(370, 1037)
+        tbx_Bank.Name = "tbx_Bank"
+        tbx_Bank.Size = New Size(200, 39)
+        tbx_Bank.TabIndex = 3
         ' 
         ' btn_Reset
         ' 
@@ -75,20 +79,55 @@ Partial Class Form1
         ' 
         btn_Close.Location = New Point(127, 1033)
         btn_Close.Name = "btn_Close"
-        btn_Close.Size = New Size(219, 46)
+        btn_Close.Size = New Size(101, 46)
         btn_Close.TabIndex = 5
         btn_Close.Text = "Close"
         btn_Close.UseVisualStyleBackColor = True
+        ' 
+        ' tbx_BetAmount
+        ' 
+        tbx_BetAmount.Location = New Point(245, 1037)
+        tbx_BetAmount.Name = "tbx_BetAmount"
+        tbx_BetAmount.Size = New Size(101, 39)
+        tbx_BetAmount.TabIndex = 6
+        ' 
+        ' cmb_RollValue
+        ' 
+        cmb_RollValue.FormattingEnabled = True
+        cmb_RollValue.Location = New Point(127, 90)
+        cmb_RollValue.Name = "cmb_RollValue"
+        cmb_RollValue.Size = New Size(171, 40)
+        cmb_RollValue.TabIndex = 7
+        ' 
+        ' cmb_RollAmount
+        ' 
+        cmb_RollAmount.FormattingEnabled = True
+        cmb_RollAmount.Location = New Point(328, 90)
+        cmb_RollAmount.Name = "cmb_RollAmount"
+        cmb_RollAmount.Size = New Size(242, 40)
+        cmb_RollAmount.TabIndex = 8
+        ' 
+        ' cmb_DifficultyLevel
+        ' 
+        cmb_DifficultyLevel.FormattingEnabled = True
+        cmb_DifficultyLevel.Location = New Point(565, 389)
+        cmb_DifficultyLevel.Name = "cmb_DifficultyLevel"
+        cmb_DifficultyLevel.Size = New Size(242, 40)
+        cmb_DifficultyLevel.TabIndex = 9
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(13F, 32F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 1191)
+        Controls.Add(cmb_DifficultyLevel)
+        Controls.Add(cmb_RollAmount)
+        Controls.Add(cmb_RollValue)
+        Controls.Add(tbx_BetAmount)
         Controls.Add(btn_Close)
         Controls.Add(btn_Reset)
-        Controls.Add(tbx_Money)
-        Controls.Add(tbx_Count)
+        Controls.Add(tbx_Bank)
+        Controls.Add(tbx_Cash)
         Controls.Add(lsb_Dice)
         Controls.Add(btn_Roll)
         Name = "Form1"
@@ -99,8 +138,12 @@ Partial Class Form1
 
     Friend WithEvents btn_Roll As Button
     Friend WithEvents lsb_Dice As ListBox
-    Friend WithEvents tbx_Count As TextBox
-    Friend WithEvents tbx_Money As TextBox
+    Friend WithEvents tbx_Cash As TextBox
+    Friend WithEvents tbx_Bank As TextBox
     Friend WithEvents btn_Reset As Button
     Friend WithEvents btn_Close As Button
+    Friend WithEvents tbx_BetAmount As TextBox
+    Friend WithEvents cmb_RollValue As ComboBox
+    Friend WithEvents cmb_RollAmount As ComboBox
+    Friend WithEvents cmb_DifficultyLevel As ComboBox
 End Class
--- DiceRoll/DiceRoll/Form1.vb
+++ DiceRoll/DiceRoll/Form1.vb
@@ -7,14 +7,42 @@
         ' Clear any previous results
         lsb_Dice.Items.Clear()
 
+        ' Holds number of time 12 is gotten for your cash each round
+        Dim count1 As Integer
+
         ' Create random number generator
         Dim rand As New Random()
 
-        ' Pay to Play
-        count -= 40
+        ' Get the bet amount from the TextBox control
+        Dim betAmount As Integer = CDec(tbx_BetAmount.Text)
+
+        ' Subtract the bet amount from the bank balance
+        count -= betAmount
+
+        ' Get the selected roll value from the ComboBox control
+        Dim rollValue As Integer = CInt(cmb_RollValue.SelectedItem)
+
+        ' Get the selected number of rolls from the ComboBox control
+        Dim rollAmount As Integer = CInt(cmb_RollAmount.SelectedItem)
+
+        ' Get the selected difficulty level from the ComboBox control
+        Dim difficultyLevel As Integer = CInt(cmb_DifficultyLevel.SelectedItem)
+
+        ' Set the reward multiplier based on the difficulty level
+        Dim rewardMultiplier As Decimal = 1.0
+        Select Case difficultyLevel
+            Case 1
+                rewardMultiplier = 1.0
+            Case 2
+                rewardMultiplier = 1.5
+            Case 3
+                rewardMultiplier = 2.0
+            Case Else
+                rewardMultiplier = 1.0
+        End Select
 
         ' Roll two dice 1200 times
-        For i As Integer = 1 To 1200
+        For i As Integer = 1 To rollAmount
             ' Roll first die
             Dim die1 As Integer = rand.Next(1, 7)
 
@@ -27,29 +55,33 @@
             ' Display result
             lsb_Dice.Items.Add("Roll " & i & ": " & die1 & " + " & die2 & " = " & total)
 
-            ' Check if we got 12
-            If total = 12 Then
-                count += 1
+            ' Check if we got the selected roll value
+            If total = rollValue Then
+                count += betAmount * 2
+                count1 += 1
             End If
         Next
 
-        ' Shows amount of cash
-        Dim JackPot As Decimal
-        JackPot = 500 * count
-        tbx_Money.Text() = JackPot
+        ' Shows amount of cash in 
+        Dim BankBal As Decimal
+        BankBal = count
+        tbx_Bank.Text() = BankBal
 
-        ' Display the number of times we got 12
-        tbx_Count.Text = count
+        ' Display the number of times we got the selected roll value
+        Dim CashBal As Decimal
+        CashBal = betAmount * count1
+        tbx_Cash.Text = "Cash Won: " & CashBal
+
     End Sub
 
     Private Sub btn_Reset_Click(sender As Object, e As EventArgs) Handles btn_Reset.Click
 
         ' Put back to starting Amount
-        Dim count As Integer = 500
+        count = 500
 
         ' TextBoxes
-        tbx_Count.Clear()
-        tbx_Money.Clear()
+        tbx_Cash.Clear()
+        tbx_Bank.Clear()
 
         ' ListBox
         lsb_Dice.Items.Clear()
@@ -65,9 +97,20 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         ' Shows amount of cash
-        Dim JackPot As Decimal
-        JackPot = 500 * count
-        tbx_Money.Text() = JackPot
+        Dim BankBal As Decimal
+        BankBal = count
+        tbx_Bank.Text() = BankBal
+
+        ' Populate the ComboBox control with the possible roll values
+        cmb_RollValue.Items.AddRange({2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+        cmb_RollValue.SelectedIndex = 0
+
+        ' Populate the ComboBox control with the amount of rolls 
+        cmb_RollAmount.Items.AddRange({10, 25, 50, 100, 250, 500, 650, 1300, 1350, 1500})
+        cmb_RollAmount.SelectedIndex = 0
+
+
+
 
     End Sub
 End Class
